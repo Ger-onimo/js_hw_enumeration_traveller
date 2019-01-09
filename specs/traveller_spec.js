93304bcd57a5a307dcef6449846f081eab80e118
Gerry Waterston <gwaterston@hotmail.co.uk>
2019-01-09 18:03:45 +0000

    Start journeys - passed

--- specs/traveller_spec.js
+++ specs/traveller_spec.js
@@ -38,7 +38,7 @@ describe('Traveller', function() {
     assert.deepStrictEqual(traveller.getJourneyStartLocations(), expected);
   });
 
-  it('should be able to get the journeys end locations', function() {
+  xit('should be able to get the journeys end locations', function() {
     const expected = [
       journey1.endLocation,
       journey2.endLocation,
@@ -49,7 +49,7 @@ describe('Traveller', function() {
     assert.deepStrictEqual(traveller.getJourneyEndLocations(), expected);
   });
 
-  it('should be able to get journeys by transport', function() {
+  xit('should be able to get journeys by transport', function() {
     const expected = [
       journey1,
       journey2
